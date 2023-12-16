b5f10fc440227e02eb11b06fd971ed4cf3fb2a07
Srikar Kale <srikar.kale01@gmail.com>
2023-12-16 11:54:32 +0530

    Web App

--- Chrome extention/index.js
+++ Chrome extention/index.js
@@ -3,7 +3,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const tabBtn = document.getElementById("tab-btn")
 const delLast = document.getElementById("delete-btn-top")
 
